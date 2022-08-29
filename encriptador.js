502b7f78fae17c0895ab03870650c658e39c2499
nicolasmartinsanchez <nicolasmartin981@gmail.com>
2022-08-29 13:10:27 -0500

    Depurados los comentarios del archivo .js

--- encriptador.js
+++ encriptador.js
@@ -1,10 +1,10 @@
 var textoIngreso = document.getElementById("cajaTexto1");   //variable que guarda la caja de texto donde el usuario debera colocar el texto que quiere encriptar o desencriptar
 var textoResultado = document.getElementById("cajaTexto2");     //variable que guarda la caja de texto donde se muestra el resultado de la encriptacion o desencriptacion
-var botonEncriptar = document.getElementById("botonEncriptar");     //variable que guarda el boton para encriptar
-var botonDesencriptar = document.getElementById("botonDesencriptar");   //variable que guarda el boton para desencriptar  
+var botonEncriptar = document.getElementById("botonEncriptar");
+var botonDesencriptar = document.getElementById("botonDesencriptar"); 
 
-var textoNormal;    //esta variable se almacena el texto sin encriptar
-var textoEncriptado;    //en esta variable se almacena el texto encriptado
+var textoNormal;
+var textoEncriptado;
 
 const vocales = ["a", "e", "i", "o", "u"];
 const reemplazoVocales = ["ai", "enter", "imes", "ober","ufat"];
@@ -22,45 +22,45 @@ var backupTexto;
 var botonCopiar;
 var botonCompartir;
 
-botonEncriptar.addEventListener("click", encriptar);    //registra el evento sobre el boton "Encriptar" que activa la funcion "encriptar" 
-botonDesencriptar.addEventListener("click", desencriptar);      //registra el evento sobre el boton "Desencriptar" que activa la funcion "desencriptar" 
+//Eventos
+botonEncriptar.addEventListener("click", encriptar);
+botonDesencriptar.addEventListener("click", desencriptar); 
 textoIngreso.addEventListener("keyup", mostrarBotonBorrar);
 botonBorrar.addEventListener("click", borrarTextoIngreso);
 
-function capturarTextoNormal()  //funcion que captura el texto normal que el usuario coloca en la caja, cuando oprime el boton "encriptar"
+function capturarTextoNormal()
 {
     textoNormal = textoIngreso.value;
 }
 
-function capturarTextoEncriptado()  //funcion que captura el texto encriptado que el usuario coloca en la caja cuando, cuando oprime el boton "desencriptar"
+function capturarTextoEncriptado()
 {
     textoEncriptado = textoIngreso.value;
 }
 
 function encriptar()
 {
-    var encontrado = false;     //esta variable se vuelve true cuando encuentra una vocal en el texto, si continua false significa que es una consonante y debe agregarse igual
+    var encontrado = false;     //esta variable se vuelve true cuando encuentra una vocal en el texto, si continua false significa que es una consonante
     textoEncriptado = "";
-    capturarTextoNormal();      //trae el texto ingresado para usarlo en esta funcion
+    capturarTextoNormal();
     for (var i = 0; i < textoNormal.length; i++)    //este ciclo recorre todo el texto ingresado por el ususario
     {
         for (j = 0; j < vocales.length; j++)    //este ciclo compara cada uno de los valores del texto ingresado con elemento del array "vocales"
         {
-            if (textoNormal[i] == vocales [j])      //se entra al condicional cuando alguna valor del texto ingresado es igual a algun elemento del array, ejemplo: a == a
+            if (textoNormal[i] == vocales [j])      //se entra al condicional cuando alguna valor del texto ingresado es igual a algun elemento del array
             {
-                textoEncriptado = textoEncriptado + reemplazoVocales[j];    //si entro al condicional quiere decir que encontro una vocal y la tiene que reemplazar por el valor encriptado que esta en el array "reemplazoVocales"
-                encontrado = true;  //true para que siga con la siguiente linea
+                textoEncriptado = textoEncriptado + reemplazoVocales[j];    //reemplaza la vocal por el valor encriptado que esta en el array "reemplazoVocales"
+                encontrado = true;
             }
         }
-        if (encontrado == false)
+        if (encontrado == false)    //este condicional quiere decir que es una consonante y debe añadirse en la variable "textoEncriptado" sin ningun cambio
         {
-            textoEncriptado = textoEncriptado + textoNormal[i];     //si entra a este condicional quiere decir que es una consonante y debe añadirse en la variable "textoEncriptado" sin ningun cambio
+            textoEncriptado = textoEncriptado + textoNormal[i];     
         }
         encontrado = false;     //se vuelve a colocar false para que en las siguientes iteraciones no de por hecho que es una vocal
     }
 
-    textoResultado.value = textoEncriptado;     //se guarda en la caja de texto la variable "textoEncriptado" para mostrar el resultado final
-    //limpiarTextoIngresado();
+    textoResultado.value = textoEncriptado;
     limpiarCajaTexto2();
     if(textoResultado.value != "")
     {
@@ -71,30 +71,30 @@ function encriptar()
 
 function desencriptar()
 {
-    capturarTextoEncriptado();      //trae el texto ingresado para usarlo en esta funcion
-    var i = 0;      //declaracion de la variable que se usa para controlar el ciclo while
+    capturarTextoEncriptado();
+    var i = 0;
     var contador = 0;   
     while (i < reemplazoVocales.length)     //ciclo que recorre el array "reemplazoVocales"
     {
         if (textoEncriptado.indexOf(reemplazoVocales[i]) >= 0)      //este condicional revisa si en el texto ingresado se encuentra algun elemento del array "reemplazoVocales", devuelve el numero de la posicion donde inicia, si es -1 significa que no hay ningun elemento del array en el texto
         {
-            var posInicial = textoEncriptado.indexOf(reemplazoVocales[i]);  //variable que guarda la posicion del texto donde inicia el array encontrado
-            var posFinal = reemplazoVocales[i].length + posInicial;   //varaible que guarda la posicion final del array encontrado, a la posicion inicial se le suma la longitud del elemento del array
-            textoEncriptado = textoEncriptado.replace(textoEncriptado.slice( posInicial, posFinal), vocales[i]);    //identifica el elemento del array encontrado dentro del texto y lo reemplaza con la vocal correspondiente, de esta manera se van reemplazando los elementos encriptados
+            var posInicial = textoEncriptado.indexOf(reemplazoVocales[i]);  //variable que guarda la posicion en el texto donde inicia el array encontrado
+            var posFinal = reemplazoVocales[i].length + posInicial;   //varaible que guarda la posicion final del array encontrado
+            textoEncriptado = textoEncriptado.replace(textoEncriptado.slice( posInicial, posFinal), vocales[i]);    //identifica el elemento del array encontrado dentro del texto y lo reemplaza con la vocal correspondiente
 
             contador++;     //este contador registra que se encontraron elementos encriptados en el texto
         }
 
         i++;
 
-        if (contador > 0 && i == 5)     //si contador es mayor que cero quiere decir que aun hay elementos encriptados en el texto y es necesario realziar el ciclo de nuevo para cambiar los que faltan, la condicion "i == 5" se coloco para que solo se realice de nuevo el ciclo una vez se haya terminado el actual 
+        if (contador > 0 && i == 5)     //si contador es mayor que cero quiere decir que aun hay elementos encriptados en el texto y es necesario realizar el ciclo de nuevo para cambiar los que faltan, la condicion "i == 5" se coloco para que solo se realice de nuevo el ciclo una vez se haya terminado el actual 
         {
             i = 0;      //i vuelve a ser cero para hacer otro ciclo
-            contador = 0;   //contador vuelve a ser 0 para registrar cambios en el texto, en caso de que no haya cambios, seguira siendo 0 y dejara acabar el ciclo
+            contador = 0;   //contador vuelve a ser 0 para registrar cambios en el texto, en caso de que no haya cambios, seguira siendo 0 y acabará el ciclo
         }
     }
-    textoResultado.value = textoEncriptado;     //se guarda en la caja de texto la variable "textoEncriptado" para mostrar el resultado final
-    //limpiarTextoIngresado();
+    
+    textoResultado.value = textoEncriptado;
     limpiarCajaTexto2();
     if(textoResultado.value != "")
     {
